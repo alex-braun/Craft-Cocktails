84fe5eaa168cb0475edb5a70ee37d5309790a58d
alex-braun <braunacb@gmail.com>
2016-09-28 09:05:12 -0400

    Add query params to cocktail search.
    Searching alphabetically or all.

---
+++ app/cocktails/index/route.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  queryParams: {
+    limit: { refreshModel: true },
+    letter: { refreshModel: true }
+  },
+
+  model(params) {
+
+    if (params.limit === 'all') {
+      return this.store.findAll('cocktail');
+    }
+
+    return this.store.query('cocktail', {
+      orderBy: 'name',
+      startAt: params.letter,
+      // endAt: params.letter,
+      endAt: params.letter+"\uf8ff"
+    });
+  },
+
+  actions: {
+
+    deleteCocktail(cocktail) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        cocktail.destroyRecord();
+      }
+    }
+  }
+
+});
