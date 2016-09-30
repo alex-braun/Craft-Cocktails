b9aee6af9785a56bda042d77962adf7b22d69115
alex-braun <braunacb@gmail.com>
2016-09-30 00:15:31 -0400

    Add style changes to fonts and bkgrnd.
    Font colors changed, navabar, background repeats.

--- app/cocktail/model.js
+++ app/cocktail/model.js
@@ -5,8 +5,8 @@ import Ember from 'ember';
 export default DS.Model.extend({
   name: DS.attr('string'),
   category: DS.attr('string'),
-  directions: DS.attr(),
-  ingredients: DS.attr(),
+  directions: DS.attr('string'),
+  ingredients: DS.attr('string'),
   imageurl: DS.attr('string'),
   user_id: DS.attr('number'),
   user: DS.attr(),
