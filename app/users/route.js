8dd70475b79ad565de779f67cf2068cf78bc90a4
alex-braun <braunacb@gmail.com>
2016-11-04 17:28:20 -0400

    Add step for deployment to gh-pages.
    Route also has had commented out code removed.

--- app/users/route.js
+++ app/users/route.js
@@ -3,16 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('cocktail');
-    // return this.get('store').findAll('cocktail');
-    // return this.get('store').find('cocktail', { editable: true });
-//     this.get('store').query('cocktail', {
-//       filter: {
-//       editable: true
-//     }
-//     }).then(function(cocktail) {
-//     return cocktail.get('cocktail');
-// });
-}
-    // results this.get('store').query('cocktail', { filter: { editable: true } });
-
+  }
 });
